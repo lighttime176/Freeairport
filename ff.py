bd321be2beeb0d20bf83f7e70e03d2d91a7484a6
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2025-01-30 18:24:31 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -1,6 +1,6 @@
 from DrissionPage import ChromiumPage
 from DrissionPage import ChromiumOptions
-import logging,random,time,requests,os
+import logging,random,time,requests,os,re
 import imaplib
 import email
 headers ={
