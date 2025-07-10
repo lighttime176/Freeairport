becb0fbd9f7b3202b96f18d988a262fc44b932ca
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2025-07-10 17:20:31 +0800

    Update hx.py

--- hx.py
+++ hx.py
@@ -71,7 +71,7 @@ ele.input("11111111")
 ele = tab.ele('text=再次输入密码')
 ele = ele.next().child()
 ele.input("11111111")
-ele = tab.ele('text=我已阅读并同意').next()
+ele = tab.ele('text=我已阅读并同意')
 logger.info(ele)
 ele.click()
 for _ in range(20):
