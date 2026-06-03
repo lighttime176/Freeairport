49630e9468b0c46bd218d20ace6f71e9a1338824
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2026-06-03 14:20:37 +0800

    Update xd.py

--- xd/xd.py
+++ xd/xd.py
@@ -336,8 +336,10 @@ try:
 except:
     pass
 
-# image_to_scan = "xd/二维码.png" 
-# data = scan_qr_native(image_to_scan)
+image_to_scan = "xd/qr_code.svg" 
+data = scan_qr_native(image_to_scan)
+with open("xd/urls.txt", "w") as file:
+    file.write(data + "\n")
 # ele = tab.ele('text=注册新账户')
 # ele1 = tab.ele('css=#confirm-register')
 # ele2 = tab.ele('css=body > div.page.page-center > div > div.card.card-md > div > div.form-footer')
